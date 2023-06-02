2d277c24fce062f772a46ce703aa6e7638c94dbf
uliadib12 <uliadib@gmail.com>
2023-06-03 00:16:10 +0700

    add testLoginSuccsess

--- tests/unit/AuthTest.php
+++ tests/unit/AuthTest.php
@@ -5,6 +5,8 @@ use CodeIgniter\Test\CIUnitTestCase;
 use CodeIgniter\Test\DatabaseTestTrait;
 use CodeIgniter\Test\FeatureTestTrait;
 
+use CodeIgniter\Shield\Entities\User;
+
 /**
  * @internal
  */
@@ -13,10 +15,39 @@ class AuthTest extends CIUnitTestCase
     use DatabaseTestTrait;
     use FeatureTestTrait;
 
+    // For Migrations
+    protected $migrate     = true;
+    protected $migrateOnce = false;
+    protected $refresh     = true;
+    protected $namespace   = 'Tests\Support';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->createDummyUser();
+    }
+
     public function testLoginSuccsess(){
         $result = $this->call('post', '/auth/login', [
             'email' => 'user@gmail.com',
             'password' => 'orangbiasa'
         ]);
+
+        $this->assertTrue($result->isOK());
+    }
+
+    private function createDummyUser(){
+        $users = auth()->getProvider();
+
+        $user = new User([
+            'username' => 'user',
+            'email'    => 'user@gmail.com',
+            'password' => 'orangbiasa',
+        ]);
+        $users->save($user);
+
+        $user = $users->findById($users->getInsertID());
+        $user->addGroup('user');
     }
 }
