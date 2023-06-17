c22f9907c351c426a9b35d1f861e665dbb48736c
uliadib12 <uliadib@gmail.com>
2023-06-17 15:51:59 +0700

    add event

--- app/Controllers/Home/HomeController.php
+++ app/Controllers/Home/HomeController.php
@@ -10,10 +10,14 @@ class HomeController extends BaseController
         $banner = glob("images/banner/*.*");
         $eventModel = new \App\Models\EventModel();
         $event = $eventModel->findAll();
+        $event_academic = $eventModel->where("tipe", "akademik")->findAll();
+        $event_non_academic = $eventModel->where("tipe", "non-akademik")->findAll();
 
         return $this->response->setJSON([
             "banner" => $banner,
-            "event" => $event
+            "event" => $event,
+            "event_academic" => $event_academic,
+            "event_non_academic" => $event_non_academic,
         ]);
     }
 }
